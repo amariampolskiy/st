c1e48e613505af08d5b08311416fbef7cd63c81d
amariampolskiy <amariampolskiy@gmail.com>
2019-06-24 13:15:32 +0300

    update man, update config

--- config.def.h
+++ config.def.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=16:antialias=true:autohint=true";
-static int borderpx = 3;
+static char *font = "Liberation Mono:pixelsize=18:antialias=true:autohint=true";
+static int borderpx = 2;
 
 /*
  * What program is execed by st depends of these precedence rules:
@@ -83,33 +83,33 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /*bg opacity*/
-float alpha = 0.9;
+float alpha = 0.95;
 
 /* Terminal colors (16 first used in escape sequence) */
+
 static const char *colorname[] = {
-	"#263238",
-	"#ff5370",
-	"#c3e88d",
-	"#d79921",
-	"#82aaff",
-	"#c792ea",
-	"#89ddff",
-	"#aebbc5",
-	"#425762",
-	"#ff5370",
-	"#c3e88d",
-	"#d79921",
-	"#82aaff",
-	"#c792ea",
-	"#89ddff",
-	"#aebbc5",
+	"#262626",
+	"#AF5F5F",
+	"#5F875F",
+	"#87875F",
+	"#5F87AF",
+	"#5F5F87",
+	"#5F8787",
+	"#6C6C6C",
+	"#444444",
+	"#FF8700",
+	"#87AF87",
+	"#FFFFAF",
+	"#8FAFD7",
+	"#8787AF",
+	"#5FAFAF",
+	"#BCBCBC",
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
 	"black",
 	"white",
 };
-
 
 /*
  * Default colors (colorname index)
@@ -150,6 +150,42 @@ static unsigned int mousebg = 0;
 static unsigned int defaultattr = 11;
 
 /*
+ * Xresources preferences to load at startup
+ */
+ResourcePref resources[] = {
+		{ "font",         STRING,  &font },
+		{ "color0",       STRING,  &colorname[0] },
+		{ "color1",       STRING,  &colorname[1] },
+		{ "color2",       STRING,  &colorname[2] },
+		{ "color3",       STRING,  &colorname[3] },
+		{ "color4",       STRING,  &colorname[4] },
+		{ "color5",       STRING,  &colorname[5] },
+		{ "color6",       STRING,  &colorname[6] },
+		{ "color7",       STRING,  &colorname[7] },
+		{ "color8",       STRING,  &colorname[8] },
+		{ "color9",       STRING,  &colorname[9] },
+		{ "color10",      STRING,  &colorname[10] },
+		{ "color11",      STRING,  &colorname[11] },
+		{ "color12",      STRING,  &colorname[12] },
+		{ "color13",      STRING,  &colorname[13] },
+		{ "color14",      STRING,  &colorname[14] },
+		{ "color15",      STRING,  &colorname[15] },
+		{ "background",   STRING,  &colorname[256] },
+		{ "foreground",   STRING,  &colorname[257] },
+		{ "cursorColor",  STRING,  &colorname[258] },
+		{ "termname",     STRING,  &termname },
+		{ "shell",        STRING,  &shell },
+		{ "xfps",         INTEGER, &xfps },
+		{ "actionfps",    INTEGER, &actionfps },
+		{ "blinktimeout", INTEGER, &blinktimeout },
+		{ "bellvolume",   INTEGER, &bellvolume },
+		{ "tabspaces",    INTEGER, &tabspaces },
+		{ "borderpx",     INTEGER, &borderpx },
+		{ "cwscale",      FLOAT,   &cwscale },
+		{ "chscale",      FLOAT,   &chscale },
+};
+
+/*
  * Internal mouse shortcuts.
  * Beware that overloading Button1 will disable the selection.
  */
@@ -175,17 +211,19 @@ static Shortcut shortcuts[] = {
 	{ XK_ANY_MOD,           XK_Print,       printsel,       {.i =  0} },
 	{ TERMMOD,              XK_Prior,       zoom,           {.f = +1} },
 	{ TERMMOD,              XK_Next,        zoom,           {.f = -1} },
+	{ TERMMOD,              XK_Up,          zoom,           {.f = +1} },
+	{ TERMMOD,              XK_Down,        zoom,           {.f = -1} },
 	{ TERMMOD,              XK_Home,        zoomreset,      {.f =  0} },
 	{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
 	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
-	{ TERMMOD,              XK_I,           iso14755,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
 	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
-	{ ShiftMask,            XK_Up,     	kscrollup,      {.i = -1} },
-	{ ShiftMask,            XK_Down,   	kscrolldown,    {.i = -1} },
+	{ ShiftMask,            XK_Up,		kscrollup,      {.i = 1} },
+	{ ShiftMask,            XK_Down,	kscrolldown,    {.i = 1} },
+	{ TERMMOD,              XK_I,           iso14755,       {.i =  0} },
     	{ TERMMOD,		XK_L,		externalpipe,	{ .v = openurlcmd } },
 };
 
